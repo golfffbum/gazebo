f78ea9eba46e2cb34b66cf3b6d243291ba2f1c71
mir <mirandamayb@gmail.com>
2020-04-01 23:45:46 +0000

    first project

---
+++ myrobot/script/hello.cpp
@@ -0,0 +1,16 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+    class WPT : public WorldPlugin
+	{
+	  public: WPT() : WorldPlugin()
+		{
+		    printf("Welcome to 's World!\n");
+		}
+	  public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+		{
+		}
+	};
+	GZ_REGISTER_WORLD_PLUGIN(WPT)
+}
